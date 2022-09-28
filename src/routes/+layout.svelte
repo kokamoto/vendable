c977757012b112b863087d485ab7a682e6696984
i866541 <kevin.okamoto@sap.com>
2022-09-28 09:13:02 -0700

    Added main layout template

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<header>
+  <nav class="container d-flex flex-wrap justify-content-center py-3 mb-4">
+    <a class="me-md-auto" href="/">Vendable</a>
+    <ul class="nav nav-pills">
+      <li class="nav-item"><a href="/about">About</a></li>
+    </ul>
+  </nav>
+</header>
+<main>
+  <div class="container"><slot></slot></div>
+</main>
+<footer></footer>
